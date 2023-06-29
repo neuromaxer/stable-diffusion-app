1360979dff2f0b0d227d1f8f9a5b3dbccf422742
neuromaxer <neuromaxed@gmail.com>
2023-06-29 19:47:38 +0200

    add api and css

--- stabledapp/src/App.js
+++ stabledapp/src/App.js
@@ -1,23 +1,75 @@
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
+import React, { Component } from "react";
+import "./index.css";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello from stable diffusion</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        const prompt = e.target.searchQuery.value;
+
+        console.log(prompt);
+        console.log(process.env.NODE_ENV);
+        document.querySelector("#overlay").style.display = "block";
+        const api =
+            process.env.NODE_ENV === "development"
+                ? "/test/stabled"
+                : "https://jma6xedjq9.execute-api.eu-central-1.amazonaws.com/test/stable-diffusion";
+        const data = { data: prompt };
+        axios({
+            method: "POST",
+            data: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+            url: api,
+        })
+            .then((response) => {
+                console.log(response);
+                const el = document.querySelector("#myImage");
+                el.setAttribute("src", response.data.body);
+                setTimeout(() => {
+                    document.querySelector("#overlay").style.display = "none";
+                    const elem = document.getElementById("searchQuery");
+                    elem.value = "";
+                    elem.focus();
+                }, 500);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    render() {
+        return (
+            <div className="container">
+                <div id="overlay">
+                    <div id="overlayText">
+                        Loading your image please wait...
+                    </div>
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <h1> Welcome to Stable Diffusion AI</h1>
+
+                    <input
+                        autoFocus={true}
+                        type="text"
+                        name="searchQuery"
+                        id="searchQuery"
+                        placeholder="enter desired text"
+                        required
+                    />
+                    <div>
+                        <input type="submit" value="Submit" />
+                    </div>
+                    <div>
+                        <br></br>
+                        <img
+                            id="myImage"
+                            alt="Your Image will appear here"
+                            className="imageContainer"
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
